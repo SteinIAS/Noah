3893cc7487477dc5e55fae6d21467f13ae3eb128
Jordan Lane <jordan.lane@steinias.com>
2015-06-19 20:16:46 +0100

    Add image minification and uglifiy

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -7,11 +7,14 @@ var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 var autoprefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 var paths = {
 	assets: {
 		js: 'js/',
-		css: 'SASS/',
+		css: './SASS/',
 		html: '../site/'
 	},
 	output: {
@@ -22,7 +25,7 @@ var paths = {
 };
 
 gulp.task('styles:dev', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src(paths.assets.css + '**/*.scss')
 		.pipe(cssGlobbing({
 			extensions: ['.css', '.scss']
 		}))
@@ -33,7 +36,7 @@ gulp.task('styles:dev', function () {
 			.pipe(sass().on('error', sass.logError))
 			.pipe(autoprefix('last 2 version', 'ie9'))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('../site/css'))
+		.pipe(gulp.dest(paths.output.css))
 		.pipe(browserSync.stream());
 });
 
@@ -50,7 +53,7 @@ gulp.task('styles:deploy', function () {
 			.pipe(autoprefix('last 2 version', 'ie9'))
 		.pipe(sourcemaps.write())
 		.pipe(minifyCss({compatibility: 'ie8'}))
-		.pipe(gulp.dest('../site/css'))
+		.pipe(gulp.dest(paths.output.css))
 		.pipe(browserSync.stream());
 });
 
@@ -64,8 +67,23 @@ gulp.task('scripts:dev', function() {
     	.pipe(gulp.dest(paths.output.js));
 });
 
+gulp.task('scripts:deploy', function() {
+	return gulp.src([paths.assets.js + 'plugins.js', paths.assets.js + 'script.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(concat('script.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.output.js));
+});
+
 gulp.task('templates', function() {
 	return browserSync.reload();
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.output.img + '**/*')
+    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+    .pipe(gulp.dest(paths.output.img));
 });
 
 gulp.task('default', ['scripts:dev', 'styles:dev'], function () {
@@ -81,4 +99,4 @@ gulp.task('default', ['scripts:dev', 'styles:dev'], function () {
 
 });
 
-gulp.task('deploy', ['styles:deploy'], function(){});
+gulp.task('deploy', ['scripts:deploy', 'styles:deploy', 'images']);
