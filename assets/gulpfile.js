0a4c7bd6f947135bad8f5334888e4fabcd25e75f
Jordan Lane <jordan.lane@steinias.com>
2015-06-19 19:07:56 +0100

    Add browser sync and default content

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -5,11 +5,13 @@ var cssGlobbing = require('gulp-css-globbing');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 
 var paths = {
 	assets: {
 		js: 'js/',
-		css: 'SASS/'
+		css: 'SASS/',
+		html: '../site/'
 	},
 	output: {
 		js: '../site/scripts/',
@@ -29,23 +31,35 @@ gulp.task('styles', function () {
 			}))
 		.pipe(sourcemaps.write())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('../site/css'));
+		.pipe(gulp.dest('../site/css'))
+		.pipe(browserSync.stream());
 });
 
-
-gulp.task('scripts', function() {
+gulp.task('scripts:dev', function() {
 	return gulp.src([paths.assets.js + 'plugins.js', paths.assets.js + 'script.js', paths.assets.js + 'dev.js'])
 	    .pipe(jshint())
     	.pipe(jshint.reporter('jshint-stylish'))
-    	.pipe(concat('script.js'))
+    	.pipe(sourcemaps.init())
+    		.pipe(concat('script.js'))
+    	.pipe(sourcemaps.write())
     	.pipe(gulp.dest(paths.output.js));
 });
 
+gulp.task('templates', function() {
+	return browserSync.reload();
+});
+
 gulp.task('default', function () {
+	browserSync.init([paths.output.js + '**/*.js'], {
+        server: "../site"
+    });
 	// Watch .js files
-	gulp.watch(paths.assets.js + '**/*.js', ['scripts']);
+	gulp.watch(paths.assets.js + '**/*.js', ['scripts:dev']);
 	// Watch .scss files
 	gulp.watch(paths.assets.css + '/**/*.scss', ['styles']);
+	// Watch .html files
+	gulp.watch(paths.assets.html + '**/*.html', ['templates']);
+
 });
 
 gulp.task('lint', function() {
