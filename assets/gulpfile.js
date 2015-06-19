def2d362dbb4f73da41f455904f63abe2cb3ebad
Jordan Lane <jordan.lane@steinias.com>
2015-06-19 16:01:49 +0100

    Minify and concatenate

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -3,6 +3,8 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var cssGlobbing = require('gulp-css-globbing');
 var sourcemaps = require('gulp-sourcemaps');
+var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 var paths = {
 	assets: {
@@ -27,15 +29,16 @@ gulp.task('styles', function () {
 			}))
 		.pipe(sourcemaps.write())
 		.pipe(sass().on('error', sass.logError))
-
 		.pipe(gulp.dest('../site/css'));
 });
 
 
 gulp.task('scripts', function() {
-	return gulp.src(paths.assets.js + '**/*.js')
+	return gulp.src([paths.assets.js + 'plugins.js', paths.assets.js + 'script.js', paths.assets.js + 'dev.js'])
 	    .pipe(jshint())
-    	.pipe(jshint.reporter('jshint-stylish'));
+    	.pipe(jshint.reporter('jshint-stylish'))
+    	.pipe(concat('script.js'))
+    	.pipe(gulp.dest(paths.output.js));
 });
 
 gulp.task('default', function () {
