bb0840c4170aa9c8e490d7c9d1fcd876ae8148ce
Ryan.davis <ryan.davis@steinias.com>
2018-06-20 16:37:41 +0100

    Added eslint config file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Default Ready Function
-var Noah = (function(){
-	// Forces Correct Operating Context >= ES2015
-    "use strict";
-    $(function() {
+const Noah = function init() {
+  // Forces Correct Operating Context >= ES2015
 
-	});
+  $(() => {
 
-})();
+  });
+};
+Noah();
