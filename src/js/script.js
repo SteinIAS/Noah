feb7fd2dfeb722111787715ab497fbb04c416895
Rohith Racherla <rohith.racherla@steinias.com>
2023-09-29 18:30:25 +0530

    Switching to named importing main sass file

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
-// import '../scss/style.scss';
+import '../scss/style.scss';
 
 // Dynamically importing all scss files using require.context
-const scssContext = require.context('styles', true, /\.scss$/);
-scssContext.keys().forEach(scssContext);
+// const scssContext = require.context('styles', true, /\.scss$/);
+// scssContext.keys().forEach(scssContext);
 
 const Noah = function init() {
   document.addEventListener('DOMContentLoaded', () => {
