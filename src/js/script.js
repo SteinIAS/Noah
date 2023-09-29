1f84014cd2d214e438f2cd12e7521131fef80d7a
Rohith Racherla <rohith.racherla@steinias.com>
2023-09-29 17:37:18 +0530

    WIP initial setup

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+import '../scss/style.scss';
+
+const Noah = function init() {
+  document.addEventListener('DOMContentLoaded', () => {
+    // everything goes here
+    alert('script loaded');
+  });
+};
+
+Noah();
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  //   mode: 'development',
+  entry: './src/js/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/script.min.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.min.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: false,
+      inject: 'body'
+    })
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+    hot: true,
+    watchFiles: ['./src/**/*.html']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              // Disables attribute quotes to match your HTML style
+              minimize: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+        generator: { filename: './img/[name][ext]' }
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: { filename: './fonts/[name][ext]' }
+      }
+    ]
+  },
+  stats: {
+    warnings: false,
+    errorDetails: true
+  }
+};
