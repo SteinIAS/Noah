da9ae043baf03ae24b6090c5037dca8ef90e662d
Rohith Racherla <rohith.racherla@steinias.com>
2023-10-03 20:18:12 +0530

    Require context to copy all images over to dist if specific cases

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,10 @@ import '../scss/style.scss';
 // const scssContext = require.context('styles', true, /\.scss$/);
 // scssContext.keys().forEach(scssContext);
 
+// Blindly copy all images over to the dist folder if any images are used in the js files and webpack doesn't pick them.
+// const imageContext = require.context('../img/', true, /\.(png|jpg|jpeg|svg)$/);
+// imageContext.keys().forEach(imageContext);
+
 const Noah = function init() {
   document.addEventListener('DOMContentLoaded', () => {
     // everything goes here
