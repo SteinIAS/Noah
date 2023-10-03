551c3c3b16a23aff44e412758fee2fe21dfce7e4
Rohith Racherla <rohith.racherla@steinias.com>
2023-10-03 16:43:09 +0530

    postcss config to automatically extract tailwind styles from @tailwind use in scss files

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,12 @@
 import '../scss/style.scss';
 
-// Dynamically importing all scss files using require.context
+// Dynamically importing all scss files using require.context if they are not imported in style.scss
 // const scssContext = require.context('styles', true, /\.scss$/);
 // scssContext.keys().forEach(scssContext);
 
 const Noah = function init() {
   document.addEventListener('DOMContentLoaded', () => {
     // everything goes here
-    alert('script loaded');
   });
 };
 
